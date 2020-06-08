3df159fb5487022fb6888d81326e2736fa982db2
Clay Arizala <clay.arizala@pucese.edu.ec>
2020-06-08 01:44:47 -0500

    Agregar archivos de proyecto.

---
+++ Listas2/Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace PilaresProgEstructurada
+{
+    partial class form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.LstNombres = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(315, 59);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(87, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Agregar";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(137, 61);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(128, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // LstNombres
+            // 
+            this.LstNombres.FormattingEnabled = true;
+            this.LstNombres.Location = new System.Drawing.Point(15, 124);
+            this.LstNombres.Name = "LstNombres";
+            this.LstNombres.Size = new System.Drawing.Size(213, 147);
+            this.LstNombres.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 64);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Ingrese un nombre";
+            // 
+            // form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.LstNombres);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListBox LstNombres;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Listas2/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PilaresProgEstructurada
+{
+    public partial class form1 : Form
+    {
+        public form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            this.LstNombres.Items.Add("Juan");
+            // 1. validar que haya datos ingresados en la caja de texto
+            // 2. agregar el nombre ingresado en el listbox (clic botón agregar)
+            // 3. el nombre se debe agregar cuando el usuario pulse enter 
+            //    en la caja de texto
+        }
+
+        private void txtNombre_TextChanged(object sender, EventArgs e)
+        {
+            if (e.KeyCode.Equals(Keys.Enter))
+            {
+                this.LstNombres.Items.Add(this.txtNombre_TextChanged.Tetx);
+                spacio de nombres  WindowsFormsApp2
+{
+     Clase parcial  pública Formlista : Formulario 
+    {
+         formlista público ()
+        {
+            InitializeComponent ();
+        }
+
+        private  void  label1_Click ( remitente de objeto  , EventArgs e ) 
+        {
+
+        }
+
+        Private  void  btnagregar_Click ( remitente de objeto  , EventArgs e ) 
+        {
+            if ( this . txtnombre . Text . Length  >  0 )
+            {
+                esta . lstNombres . Artículos . Agregar ( this . Txtnombre . Text );
+                esta . txtnombre . Texto  =  " " ;
+            }
+        }
+
+        private  void  text_TextChanged ( remitente del objeto  , EventArgs e ) 
+        {
+            
+        }
+
+        privado  vacío  btnagregar_KeyPress ( remitente de objeto  , KeyPressEventArgs e ) 
+        {
+           
+        }
+
+        privado  vacío  txtnombre_KeyPress ( remitente de objeto  , KeyPressEventArgs e ) 
+        {
+            
+
+        }
+
+         nulo  privado txtnombre_KeyDown ( remitente del objeto  , KeyEventArgs e ) 
+        {
+            if ( e . KeyCode . Equals ( Keys . Enter ))
+            {
+                if ( this . txtnombre . Text . Length  >  0 )
+                {
+                    esta . lstNombres . Artículos . Agregar ( this . Txtnombre . Text );
+                    esta . txtnombre . Texto  =  " " ;
+                }
+            }
+        }
+    }
+}
+
+           
+
+
+        }
+    }
+}
